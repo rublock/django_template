8f7bc29046744602879c1ab445e8b590c94c697f
Mack <hypermail@yandex.ru>
2024-05-19 17:23:59 +0300

    first_template

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class MainPageView(TemplateView):
+    """Главная страница"""
+
+    template_name = "base.html"
