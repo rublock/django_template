99c7a4fcecd441f93f6b4cf659a4bdf7365ab195
Mack <hypermail@yandex.ru>
2024-05-19 17:58:24 +0300

    cat_api

---
+++ mainapp/services/cat_api.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def get_cat():
+    response = requests.get(
+        "https://api.thecatapi.com/v1/images/search",
+        params={
+            "size": "small",
+            "mime_types": "jpg",
+            "format": "json",
+            "has_breeds": "true",
+            "order": "RANDOM",
+            "page": "0",
+            "limit": "1",
+        },
+    )
+    data = response.json()
+    cat_url = data[0]["url"]
+    return cat_url
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,15 @@
 from django.views.generic import TemplateView
 
+from .services.cat_api import get_cat
+
 
 class MainPageView(TemplateView):
     """Главная страница"""
 
     template_name = "base.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cat_url = get_cat()
+        context["cat_url"] = cat_url
+        return context
