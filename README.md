b5f7e1d844671828b85ea66922b8badbce688996
Mack <hypermail@yandex.ru>
2024-05-22 13:47:37 +0300

    MainPageViewTest, README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# django_CBV
-for uderstanding Django CBV
+### BASE DJANGO TEMPLATE
+##### Mainapp
+
+* .gitignore python
+* MIT License
+* mainapp
+* .env and sample.env
+* BASE_DIR / "templates"
+* mainapp/services
+* requirements/local.txt
+* bootstarp5, static, header, sticky footer 
+* MainPageViewTest
--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from mainapp.views import MainPageView
 
-# Create your tests here.
+
+class MainPageViewTest(TestCase):
+    def test_context_data(self):
+        view = MainPageView()
+        context = view.get_context_data()
+        self.assertIn("cat_img", context)
+        self.assertIn("title", context)
+        self.assertEqual(context["title"], "cats")
